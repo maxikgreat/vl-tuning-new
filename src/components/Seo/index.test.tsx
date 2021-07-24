591ffe6e10465eeff1a666199ee8ac25180c75b2
Maksym Vasylenko <maksym.vasylenko@polcode.net>
2021-07-24 17:19:37 +0200

    add tests to Seo

---
+++ src/components/Seo/index.test.tsx
@@ -0,0 +1,23 @@
+import {render} from '@testing-library/react';
+
+import {Seo} from './index';
+import {getTitle, getDescription, titleSuffix, defaultTitle, defaultDescription} from './utils';
+
+describe('Seo', () => {
+	describe('utils', () => {
+	  it('should work correctly', () => {
+			expect(getTitle('Awesome Title')).toBe('Awesome Title' + titleSuffix);
+			expect(getTitle()).toBe(defaultTitle + titleSuffix);
+
+			expect(getDescription('Some awesome description')).toBe('Some awesome description');
+			expect(getDescription()).toBe(defaultDescription);
+		});
+	});
+
+	describe('component', () => {
+	  it('should render correctly', () => {
+	    expect(render(<Seo />).baseElement).toBeInTheDocument();
+		});
+	});
+});
+
--- src/components/Seo/utils.ts
+++ src/components/Seo/utils.ts
@@ -1,7 +1,7 @@
-const titleSuffix = ' | VL-Tuning';
+export const titleSuffix = ' | VL-Tuning';
 
-const defaultTitle = 'Lorem ipsum';
-const defaultDescription = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dolores ea eum inventore ipsa ipsam non omnis reiciendis tempore tenetur.';
+export const defaultTitle = 'Lorem ipsum';
+export const defaultDescription = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dolores ea eum inventore ipsa ipsam non omnis reiciendis tempore tenetur.';
 
-export const getTitle = (text? = defaultTitle): string => `${text}${titleSuffix}`;
-export const getDescription = (description? = defaultDescription): string => description;
+export const getTitle = (text = defaultTitle): string => `${text}${titleSuffix}`;
+export const getDescription = (description = defaultDescription): string => description;
