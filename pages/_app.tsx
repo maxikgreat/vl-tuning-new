07a36dd7358f55375d1c489d1f40a9d9c6ed7f23
Maksym Vasylenko <maksym.vasylenko@polcode.net>
2021-07-25 19:47:49 +0200

    add progress page loader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import 'styles/globals.css';
 import type {AppProps} from 'next/app';
+import NextNprogress from 'nextjs-progressbar';
 
 function MyApp({Component, pageProps}: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<NextNprogress
+				color="#7f81c0"
+				startPosition={0.3}
+				stopDelayMs={200}
+				height={3}
+				showOnShallow={true}
+				options={{showSpinner: false}}
+			/>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import {VFC} from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import Logo from 'images/icons/logo.svg';
 import {LocationInfo} from 'components/Header/LocationInfo';
@@ -7,12 +8,14 @@ import {LocationInfo} from 'components/Header/LocationInfo';
 export const Header: VFC = () => (
 	<header className="flex justify-between items-center p-4">
 		<LocationInfo isMobile={false} />
-		<div className="w-24 sm:w-28 md:w-32 lg:w-36">
-			<Image src={Logo} alt="company logo" placeholder="empty" />
-		</div>
+		<Link href="/">
+			<div className="w-24 sm:w-28 md:w-32 lg:w-36">
+				<Image src={Logo} alt="company logo" placeholder="empty" />
+			</div>
+		</Link>
 		<div className="text-sm sm:text-base">
-			<h3 className="mb-0 sm:mb-3"><a href="tel:+380972713473">+380 97 271 34 73</a></h3>
-			<h3 className="mb-3 sm:mb-0"><a href="mailto:vltuning@gmail.com">vltuning@gmail.com</a></h3>
+			<h3 className="mb-0 sm:mb-3"><a href="tel:+380972713473" rel="nofollow noopener noreferrer">+380 97 271 34 73</a></h3>
+			<h3 className="mb-3 sm:mb-0"><a href="mailto:vltuning@gmail.com" rel="nofollow noopener noreferrer">vltuning@gmail.com</a></h3>
 			<LocationInfo isMobile={true} />
 		</div>
 	</header>
